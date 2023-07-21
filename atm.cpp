3c480d2aeea90aec015e46d75bc686847691c767
imgsude <115190068+imgsude@users.noreply.github.com>
2023-07-21 15:08:24 +0300

    Add files via upload
    basic atm code with cpp

---
+++ atm.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+int main(){
+    /*int secim;
+    int bakiye{1000};
+    int tutar;
+do{
+    cout<<"ATM'mize hosgeldiniz!"<<endl;
+    cout<<"Lutfen yapmak istediginiz islemi seciniz:\n1-Bakiye sorgulama \n2-Para cekme\n3-Para yatirma\n4-cikis"<<endl;
+    cout<<"Seciminiz:";
+    cin>>secim;
+    if(secim=1){ cout<<"Bakiyeniz: "<<bakiye<<endl;}
+
+    else if(secim=2){cout<<"Su anki bakiyeniz: "<<bakiye<<endl;
+                  cout<<"Cekmek istediginiz tutari giriniz: ";
+                  cin>>tutar;
+                  bakiye=bakiye-tutar;
+                  cout<<"Yeni bakiyeniz: "<<bakiye<<endl;}
+        
+     else if(secim=3){cout<<"Su anki bakiyeniz: "<<bakiye<<endl;
+                  cout<<"Yatirmak istediginiz tutari giriniz: ";
+                  cin>>tutar;
+                  bakiye=bakiye+tutar;
+                  cout<<"Yeni bakiyeniz: "<<bakiye<<endl;}
+        
+                 
+        
+    }while(secim!=4);
+        cout<<endl;
+    */
+
+    return 0;
+}
